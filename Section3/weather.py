a38f02e237308bb4123c5e7dc7097da0414f581a
Craig Murphy <craigmurphy40@gmail.com>
2024-06-04 14:47:52 -0400

    adding Python 3 fundamentals section 2 - 4

---
+++ Section3/weather.py
@@ -0,0 +1,7 @@
+temperature = 75
+forecast = "rainy"
+
+if temperature < 80 and forecast != "rainy":
+  print("Go outside!")
+else:
+  print("Have a good day!")
---
+++ Section4/expenses.py
@@ -0,0 +1,9 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+#sum = 0
+
+#for i in expenses:
+#  sum = sum + i
+
+total = sum(expenses)
+
+print('You spent $', total, sep = '')
