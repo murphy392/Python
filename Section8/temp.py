e7634211c4d85328f8ae1e10b11700296cb6e15a
murphy392 <craigmurphy40@gmail.com>
2024-06-28 13:51:36 -0400

    Delete Section8 directory

--- Section8/temp.py
+++
@@ -1,6 +0,0 @@
-acronym = input('What acronym do you want to add?\n')
-definition = input('What is the definition?\n')
-with open('acronyms.txt', 'a') as file:
-    file.write(acronym + ' - ' + definition + '\n')
-# Write the acronym and definition to the file
-
