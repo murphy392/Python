3a244cdebe6bfa7985e0c78ca87b802b46ced7e9
Craig Murphy <craigmurphy40@gmail.com>
2024-06-28 14:43:21 -0400

    finishing section3

--- Working_With_Files/Section3/demos/M3_Working_with_Files_and_Folders/04_Demo_Moving_Files.py
+++ Working_With_Files/Section3/demos/M3_Working_with_Files_and_Folders/04_Demo_Moving_Files.py
@@ -3,4 +3,6 @@ import shutil
 def mv_files(src, dst):
     shutil.move(src, dst)
 
-mv_files('./files/text.txt', './files/subfolder/text.txt')
+# mv_files('./files/text.txt', './files/subfolder/text.txt')
+# mv_files('./files', '/.xyz')
+mv_files('./xyz', './files')
---
+++ Working_With_Files/Section3/demos/M3_Working_with_Files_and_Folders/06_Demo_Deleting_Files.py
@@ -0,0 +1,12 @@
+import os
+
+def remove_file(f):
+    if os.path.isfile(f):
+        try:
+            os.remove(f)
+        except OSError as e:
+            print(f'Error: {f} : {e.strerror}')
+    else:
+        print(f'Error: {f} is not a valid file')
+
+remove_file('./files/text.txt')
