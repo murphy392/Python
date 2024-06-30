f1db525c2dc616c3921efc567f5d01b6a59489b3
Craig Murphy <craigmurphy40@gmail.com>
2024-06-30 17:16:06 -0400

    Finished Working_With_Files

---
+++ Working_With_Files/Section5/demos/M5_Reading_and_Writing_Files/04_Demo_Working_with_JSON_Files.py
@@ -0,0 +1,16 @@
+import json
+
+def read_print_json(fn, pretty, sort):
+    with open(fn) as json_file:
+        data = json.load(json_file)
+        print(json.dumps(data,  sort_keys=sort, indent=4) if pretty else data)
+
+def update_author_json(fn, arr_name, pos, key, value):
+    with open(fn, 'r') as read_file:
+        data = json.load(read_file)
+        data[arr_name][pos][key] = value
+        with open(fn, 'w') as write_file:
+            json.dump(data, write_file)
+
+# read_print_json('./files_to_read/authors.json', False, False)
+update_author_json('./files_to_read/authors.json', 'authors', 1, 'courses', 10)
---
+++ Working_With_Files/Section5/demos/M5_Reading_and_Writing_Files/05_Demo_Persisting_Objects.py
@@ -0,0 +1,39 @@
+#serialize and save the class in a binary file
+import pickle
+
+class Person:
+    age = 45
+    name = 'John Smith'
+    kids = ['Pete', 'Lilly', 'Kate']
+    employers = {'AWS' : 2022, 'Microsoft': 2018, 'Yahoo': 2005}
+    shoe_sizes = (11, 12)
+
+def serialize(obj) -> pickle:
+    pickled = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
+    print(f'Serialized object: \n{pickled}\n')
+    return pickled
+
+def deserialize(obj):
+    unpickled = pickle.loads(obj)
+    print(f'Deserialized: \n{unpickled}')
+
+def deserialize_employers(obj):
+    unpickled = pickle.loads(obj)
+    print(f'Deserialized employers: \n{unpickled.employers}\n')
+
+def obj_to_file(fn, obj):
+    with open(fn, 'wb') as pf:
+        pickle.dump(obj, pf, protocol=pickle.HIGHEST_PROTOCOL)
+
+def file_to_obj(fn, obj) -> object:
+    with open(fn, 'rb') as pf:
+        obj = pickle.load(pf)
+        print(obj)
+        return obj
+
+# pickled = serialize(Person())
+# deserialize(pickled)
+# deserialize_employers(pickled)
+
+obj = obj_to_file('./files_to_read/person.xyz', Person())
+person = file_to_obj('./files_to_read/person.xyz', obj)
